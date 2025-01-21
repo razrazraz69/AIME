9f2a2ac9c217163cbc68cdfd6e7a05dfe9e6e91b
razrazraz <92352593+razrazraz69@users.noreply.github.com>
2025-01-21 15:38:03 +0700

    add AikaVerse

---
+++ contracts/AikaVerseToken.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
+
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { OFT } from "@layerzerolabs/oft-evm/contracts/OFT.sol";
+
+contract AikaVerseToken is OFT {
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        address _lzEndpoint,
+        address _delegate
+    ) OFT(_name, _symbol, _lzEndpoint, _delegate) Ownable(_delegate) {
+        if (block.chainid == 8453) {
+            _mint(msg.sender, 1_000_000_000 * 10 ** decimals());
+        }
+    }
+}
